a3a29a319b31f3543affa8d883157cbe555b6892
chenaoh <cdhenaogmail.com>
2024-10-31 00:12:24 -0500

    denuncia

---
+++ src/test/java/com/barturenrivera/pruebanivel/PnlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barturenrivera.pruebanivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
